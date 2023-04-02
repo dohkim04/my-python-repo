23624c915c219eef9f1f744ddebc6397007d994c
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 21:34:51 +0000

    4. Remove two specific services from the list by name or by index

--- wk12-project_AWS-service-inventory.py
+++ wk12-project_AWS-service-inventory.py
@@ -5,49 +5,52 @@
 
 # Create a list of services using Python. ex. S3, Lambda, EC2, etc.
 
-# Instruction:
-print("==========")
+############## Instruction ######################
+#1. The initialization of a AWS Service List
+print()
 print("1. The list should be empty initially.")
 aws_service_list = []
 print("The initial status of aws_service_list:", aws_service_list)
-print("\n")
+print("==========\n")
 
 # Output:
 # ~/environment/my-python-repo (week12-mini-project) $ python3.7 wk12-project_AWS-service-inventory.py 
 # []
 
-
+#################################################
+#2. Populate the list using append or insert
 print("2. Populate the list using append or insert.")
 aws_service_list.append('EC2')
 aws_service_list.append('Lambda')
 aws_service_list.append('Lightsail')
 aws_service_list.append('ECR')
 aws_service_list.append('ECS')
-print(" - The initial service list using append:", aws_service_list)
+print(" # Services were added at the end of the list using append:\n  {}", aws_service_list) 
+# 'append' will add each element at the end of the list
 
 aws_service_list.insert(3,'S3')
-print(" - After inserting 'S3' at index 3:", aws_service_list)
+print(" # After inserting 'S3' at index 3:\n", (aws_service_list))
 
 aws_service_list.insert(5,'EFS')
-print(" - After inserting 'EFS' at index 6:", aws_service_list)
+print(" # After inserting 'EFS' at index 6:\n", (aws_service_list))
 
 aws_service_list.insert(-1,'Glacier')
-print(" - After inserting 'Goacier' before index -1:", aws_service_list)
+print(" # After inserting 'Goacier' before index -1:\n", (aws_service_list))
 
 aws_service_list.insert(0, 'VPC')
-print(" - After inserting 'VPC' at index 0:", aws_service_list)
+print(" - After inserting 'VPC' at index 0:\n", (aws_service_list))
 
 aws_service_list.insert(-4,'AppSync')
-print(" - After inserting 'AppSync' before index -4:", aws_service_list)
+print(" - After inserting 'AppSync' before index -4:\n", (aws_service_list))
 
-print("==========")
 # added comments
-print("## Note ##")
-print("When using a minus index number, a new element will be added before the designated index number")
-print('When using a positive index number, a new element will be added at the designated index number')
+
+# print("\n## Note ##")
+# print("When using a minus index number, a new element will be added 'BEFORE' the designated index number")
+# print('When using a positive index number, a new element will be added at the designated index number')
 print("==========\n")
 
-# Printed output
+# Printed the output for checking in the middle of the project
 '''
 ~/environment/my-python-repo (week12-mini-project) $ python3.7 wk12-project_AWS-service-inventory.py 
 []
@@ -62,12 +65,23 @@ When using a minus index number, a new element will be added before the designat
 When using a positive index number, a new element will be added at the designated index number
 '''
 
+##########################################################
+#3. Print the list and the length of the list
 print("3. Print the list and the length of the list.")
 print(' - AWS Service List:', aws_service_list)
 print(' - The length of the list:', len(aws_service_list))
+print("==========\n")
+
+####################################################################
+#4. Remove two specific services from the list by name or by index.
+print("4. Remove two specific services from the list by name or by index.")
+
+aws_service_list.remove('Lightsail')
+print("After removing 'Lightsail' from the list:", aws_service_list)
+del aws_service_list[6]
+print("After removing an element at index 6 from the list:", aws_service_list)
 
 """
-4. Remove two specific services from the list by name or by index.
 5. Print the new list and the new length of the list.
 6. Push your code to GitHub and include the link in your LinkedIn write-up.
 """
