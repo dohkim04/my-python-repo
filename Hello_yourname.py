a3deef3f93cd7e8cf9d1fbd99318bd05073833ad
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 16:53:50 +0000

    Hello_yourname function

---
+++ Hello_yourname.py
@@ -0,0 +1,31 @@
+# You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+# Output:
+# Hello firstname lastname! You just delved into python.
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+'''
+Sample Input 0
+==>
+Ross
+Taylor
+
+Sample Output 0
+==>
+Hello Ross Taylor! You just delved into python.
+'''
+# use format function
+
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello {} {}! You just delved into python.".format(first, last))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
