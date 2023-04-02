f5b053357ea6a7dde79787353c4de07d07e77cae
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 03:37:06 +0000

    change the file attribute as executable and test function

--- variations.py
+++ variations.py
@@ -9,3 +9,11 @@ print("Lowercase:", message.lower())
 
 #3. make all uppercase
 print("Uppercase:", message.upper())
+
+# Revo@UwordI:~/environment/my-python-repo (dkim-04012023-v3) $ chmod u+x variations.py 
+# Revo@UwordI:~/environment/my-python-repo (dkim-04012023-v3) $ ./variations.py 
+# Enter a message: sdggs
+# Revo@UwordI:~/environment/my-python-repo (dkim-04012023-v3) $ ./variations.py 
+# Enter a message: This is a testing message.
+# Lowercase: this is a testing message.
+# Uppercase: THIS IS A TESTING MESSAGE.
