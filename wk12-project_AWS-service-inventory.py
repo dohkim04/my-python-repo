76a7e6cb5f4bed839e1c824883fc65b3c826c3b9
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 20:56:40 +0000

    print the list and the length of the list

--- wk12-project_AWS-service-inventory.py
+++ wk12-project_AWS-service-inventory.py
@@ -6,38 +6,44 @@
 # Create a list of services using Python. ex. S3, Lambda, EC2, etc.
 
 # Instruction:
-# 1. The list should be empty initially.
+print("1. The list should be empty initially.")
 aws_service_list = []
-print(aws_service_list)
+print("The initial status of aws_service_list:", aws_service_list)
 
 # Output:
 # ~/environment/my-python-repo (week12-mini-project) $ python3.7 wk12-project_AWS-service-inventory.py 
 # []
 
 
-# 2. Populate the list using append or insert.
+print("2. Populate the list using append or insert.")
 aws_service_list.append('EC2')
 aws_service_list.append('Lambda')
 aws_service_list.append('Lightsail')
 aws_service_list.append('ECR')
 aws_service_list.append('ECS')
-print("The initial service list:", aws_service_list)
+print("The initial service list using append:", aws_service_list)
+
 aws_service_list.insert(3,'S3')
-print("After adding 'S3' at index 3:", aws_service_list)
+print("After inserting 'S3' at index 3:", aws_service_list)
+
 aws_service_list.insert(5,'EFS')
-print("After adding 'EFS' at index 6:", aws_service_list)
+print("After inserting 'EFS' at index 6:", aws_service_list)
+
 aws_service_list.insert(-1,'Glacier')
-print("After adding 'Goacier' before index -1:", aws_service_list)
+print("After inserting 'Goacier' before index -1:", aws_service_list)
+
 aws_service_list.insert(0, 'VPC')
-print("After adding 'VPC' at index 0:", aws_service_list)
+print("After inserting 'VPC' at index 0:", aws_service_list)
+
 aws_service_list.insert(-4,'AppSync')
-print("After adding 'AppSync' before index -4:", aws_service_list)
-# print(aws_service_list) # Check the content of this list.
+print("After inserting 'AppSync' before index -4:", aws_service_list)
+
+# added comments
 print("Note:")
 print("When using a minus index number, a new element will be added before the designated index number")
 print('When using a positive index number, a new element will be added at the designated index number')
 
-# Output
+# Printed output
 '''
 ~/environment/my-python-repo (week12-mini-project) $ python3.7 wk12-project_AWS-service-inventory.py 
 []
@@ -53,12 +59,15 @@ When using a positive index number, a new element will be added at the designate
 '''
 
 # 3. Print the list and the length of the list.
+print('\n=================================\nAWS Service List: ', aws_service_list)
+print("The length of the aws_service_list: ", len(aws_service_list))
 
 """
 4. Remove two specific services from the list by name or by index.
 5. Print the new list and the new length of the list.
 6. Push your code to GitHub and include the link in your LinkedIn write-up.
 """
+
 '''
 Below is a selected list of common AWS services below.
 Compute (EC2, Lightsail, Lambda, EB, Outposts)
