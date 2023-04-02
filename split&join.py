721faa5868f7e47d7b4067e69b07964d53640212
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 17:40:53 +0000

    adjust print statement

--- split&join.py
+++ split&join.py
@@ -16,10 +16,20 @@ def split_and_join(line):
     # write your code here
     word_list = line.split(" ")  # split each word by a space, " "
     print("Word list:", word_list)
-    return "-".join(word_list) # join each word by a "-"
+    return "-".join(word_list) # join each word by a "-" to create a new string and return it.
 
+    # Previous wrong statements below:
+    # word_list = line.split(" ").join("-")
+    # Error message:
+    # AttributeError: 'list' object has no attribute 'join'
+    
 if __name__ == '__main__':
     line = input("Enter your string: ")
     result = split_and_join(line)
-    print(result)
+    print("Split and join statement:", result)
     
+
+# Revo@UwordI:~/environment/my-python-repo (dkim-04022023-python-string-split-and-join) $ ./split\&join.py 
+# Enter your string: I am going to test this split and join function.
+# Word list: ['I', 'am', 'going', 'to', 'test', 'this', 'split', 'and', 'join', 'function.']
+# 
