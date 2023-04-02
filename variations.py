27a987c409f1c598db8f1ed19beab22563ffc264
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-02 03:33:36 +0000

    receive a message then print the string in all lowercase and all uppercase

---
+++ variations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+# Python implementation
+#1. receive a message
+message = input("Enter a message: ")
+
+#2. make all lowercase 
+print("Lowercase:", message.lower())
+
+#3. make all uppercase
+print("Uppercase:", message.upper())
