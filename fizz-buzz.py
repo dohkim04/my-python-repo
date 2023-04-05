61a77351b0e08ed3cc1452ea97113c8e24228a31
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-05 02:25:48 +0000

    testing for loop

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3.7
 # accept an integer input from a user
-user_input = input("Enter an integer number: ")
+user_input = int(input("Enter an integer number: "))
 
 # create a range from 1 to the user's provided number
 # and loop through it
 for number in range(1, user_input+1): # the last number is not included in range, Thus need to plus 1
     print(number)
-    
-# print FizzBuzz if the value is a multiple of 3 and 5
-    if (number % 3 == 0) and (number % 5 == 0):
+"""
+    if (number % 3 == 0) and (number % 5 == 0): 
         print("FizzBuzz")
 # print Fizz if the value is a multiple of 3
     elif number % 3 == 0:
@@ -20,3 +19,4 @@ for number in range(1, user_input+1): # the last number is not included in range
     else:
         print(number)
     
+"""
