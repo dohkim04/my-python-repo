c6a2716dc4429f36391d671db5d3aa8035051947
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-05 02:12:14 +0000

    fizz buzz program draft ver1

---
+++ fizz-buzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+# accept an integer input from a user
+user_input = input("Enter an integer number: ")
+
+# create a range from 1 to the user's provided number
+# and loop through it
+user_range = range(1, user_input+1) # the last number is not included in range, Thus need to plus 1
+
