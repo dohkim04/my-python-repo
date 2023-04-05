d4c4d720935f245543a9a8be122d6d8caba5aad9
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-05 02:20:13 +0000

    Fix syntax error

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -4,5 +4,18 @@ user_input = input("Enter an integer number: ")
 
 # create a range from 1 to the user's provided number
 # and loop through it
-user_range = range(1, user_input+1) # the last number is not included in range, Thus need to plus 1
-
+for number in range(1, user_input+1): # the last number is not included in range, Thus need to plus 1
+    print(number)
+    
+# print FizzBuzz if the value is a multiple of 3 and 5
+elif number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+# print Fizz if the value is a multiple of 3
+elif number % 3 == 0:
+    print("Fizz")
+# print Buzz if the value is a multiple of 5
+elif number % 5 == 0:
+    print("Buzz")
+# print the number as it is if nothing above applies.
+else:
+    print(number)
