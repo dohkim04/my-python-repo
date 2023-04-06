7e94229fabe95566121299ecdc96732378999a5d
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-05 23:18:37 -0400

    added blank lines

--- wk13-Mon-lesson-04032023.py
+++ wk13-Mon-lesson-04032023.py
@@ -8,6 +8,7 @@ print(var2)
 # ~/my-python-repo$ python3.10 wk13-Mon-lesson-04032023.py 
 # <class 'list'>
 # [251, 386, 493]
+print("\nblank line intentionally\n")
 
 var3 = [251, 386, 493, '009']
 print(var3)
@@ -19,3 +20,10 @@ print(dir(var3))
 numbers = [1,2,3,4,5]
 print(numbers)
 
+print("\nblank line intentionally\n")
+# range 
+print(range(1, 101)) # print from 1 up to 100 (last number is not included)
+r = range(1,101)
+print(list(r))
+
+print("\nblank line intentionally\n")
