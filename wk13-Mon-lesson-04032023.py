60f9e859e5263392d33b03497072e861a6a50e47
Do Hyung Kim <doh.kim04@gmail.com>
2023-04-05 23:09:45 -0400

    Working on list data types

---
+++ wk13-Mon-lesson-04032023.py
@@ -0,0 +1,20 @@
+# 13th week Python evening call review
+
+var=[]
+print(type(var)) 
+var2 = [251, 386, 493]
+print(var2)
+# output
+# ~/my-python-repo$ python3.10 wk13-Mon-lesson-04032023.py 
+# <class 'list'>
+# [251, 386, 493]
+
+var3 = [251, 386, 493, '009']
+print(var3)
+var3.append(649)
+print(var3)
+print(dir(var3))
+
+numbers = [1,2,3,4,5]
+print(numbers)
+
